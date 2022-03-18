65ead8111febc049c71592576902000bf2515d03
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-18 09:59:25 +0100

    il était une fois

---
+++ app/src/main/java/education/cccp/tp3android/MainActivity.java
@@ -0,0 +1,14 @@
+package education.cccp.tp3android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
