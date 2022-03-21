2f54f542276260d64373ee854027a66159b86467
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-21 13:30:15 +0100

    création d'un second_activity.xml en constraintLayout

--- app/src/main/java/education/cccp/basics/MainActivity.java
+++ app/src/main/java/education/cccp/basics/MainActivity.java
@@ -1,20 +1,21 @@
 package education.cccp.basics;
 
-import static education.cccp.basics.R.layout.activity_main;
+
+import static education.cccp.basics.R.layout.second_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity_main);
+//        setContentView(education.cccp.basics.R.layout.activity_main);
+        setContentView(second_activity);
     }
 
     public void editTextDateOnClickEventListener(View view) {
