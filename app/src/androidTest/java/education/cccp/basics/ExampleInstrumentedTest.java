0eed7e7d63c80331696a99ca316b08c07ba6e4ae
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-21 10:53:19 +0100

    ajouter de l'image.

--- app/src/androidTest/java/education/cccp/basics/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/education/cccp/basics/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("education.cccp.tp3android", appContext.getPackageName());
+
     }
 }
